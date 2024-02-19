0b052159b9705384a37ad073c375a9e2d7c79c37
RO35ERT <trebrorobertsbob@gmail.com>
2024-02-19 15:44:53 +0200

    updated

--- core/settings.py
+++ core/settings.py
@@ -229,3 +229,12 @@ REST_AUTH = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000/",
 ]
+
+
+##send email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')
