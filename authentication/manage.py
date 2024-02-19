956cb24b41137b78d14088684e42d057edc43af7
RO35ERT <trebrorobertsbob@gmail.com>
2024-02-19 16:06:18 +0200

    updated

---
+++ authentication/manage.py
@@ -0,0 +1,59 @@
+from django.contrib.auth.model import BaseUserManager
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+from django.utils.translation import gettext_lazy as _
+
+class CustomUserManager(BaseUserManager):
+    def email_validation(self, email):
+        try:
+            validate_email(email)
+        except ValidationError:
+            raise ValidationError(_('Invalid email'))
+        
+    def create_user(self, email,first_name,last_name=None, password=None, **extra_fields):
+        if not email:
+            raise ValueError(_("This is a required field"))
+        else:
+            self.email_validation(email)
+            cleaned_email = self.normalize_email(email)
+            if not first_name:
+                raise ValueError(_("This is a required field"))
+            
+            user = self.model(
+                email=cleaned_email,
+                first_name=first_name,
+                last_name=last_name,
+                **extra_fields
+            )
+            
+            user.set_password(password)
+            extra_fields.setdefault('is_staff', False)
+            extra_fields.setdefault('is_superuser', False)
+            user.save()
+            
+            return user
+        
+    def create_superuser(self, email, first_name, last_name=None, password=None, **extra_fields):
+        if not email:
+            raise ValueError(_("This is a required field"))
+        else:
+            self.email_validation(email)
+            cleaned_email = self.normalize_email(email)
+            if not first_name:
+                raise ValueError(_("This is a required field"))
+
+            user = self.model(
+                email=cleaned_email,
+                first_name=first_name,
+                last_name=last_name,
+                **extra_fields
+            )
+
+            user.set_password(password)
+            extra_fields.setdefault('is_staff', True)
+            extra_fields.setdefault('is_superuser', True)
+            
+            user = self.create_user(email, first_name, last_name, password, **extra_fields)
+            user.save()
+
+            return user
--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, PermissionsMixin, BaseUserMixin
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+class CustomUserModel(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique=True)
+    first_name = models.CharField(_('first name'), max_length=150, blank=True)
+    last_name = models.CharField(_('last name'), max_length=150, blank=True)
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin site.'),
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
+    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
+    last_login = models.DateTimeField(_('date joined'), auto_now_add=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name']
