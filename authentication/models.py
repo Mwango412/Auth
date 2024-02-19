543b8e9c54a4fa2bc32079f9bf18a42c77eda28e
RO35ERT <trebrorobertsbob@gmail.com>
2024-02-19 16:13:03 +0200

    updated

--- authentication/models.py
+++ authentication/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, PermissionsMixin, BaseUserMixin
 from django.utils.translation import gettext_lazy as _
-
+from .managers import CustomUserManager
 # Create your models here.
 class CustomUserModel(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_('email address'), unique=True)
@@ -25,3 +25,8 @@ class CustomUserModel(AbstractBaseUser, PermissionsMixin):
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name']
+    
+    objects = CustomUserManager()
+    
+    def __str__(self):
+        return self.email
