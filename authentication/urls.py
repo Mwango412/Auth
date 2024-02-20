4ba2849f2dea4909938d1645f4c528d8b31d72b7
RO35ERT <trebrorobertsbob@gmail.com>
2024-02-20 09:55:13 +0200

    updated

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,15 +3,16 @@ from dj_rest_auth.views import LoginView, LogoutView, UserDetailsView
 from allauth.socialaccount.views import signup
 from authentication.views import GoogleLogin
 from authentication.views import FacebookLogin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 
 urlpatterns = [
-    path("register/", RegisterView.as_view(), name="rest_register"),
-    path("login/", LoginView.as_view(), name="rest_login"),
-    path("logout/", LogoutView.as_view(), name="rest_logout"),
-    path("user/", UserDetailsView.as_view(), name="rest_user_details"),
+    # path("register/", RegisterView.as_view(), name="rest_register"),
+    # path("login/", LoginView.as_view(), name="rest_login"),
+    # path("logout/", LogoutView.as_view(), name="rest_logout"),
+    # path("user/", UserDetailsView.as_view(), name="rest_user_details"),
+    path('rest/', include('dj_rest_auth.urls')),
     path("signup/", signup, name="socialaccount_signup"),
     path("google/", GoogleLogin.as_view(), name="google_login"),
     path("facebook/", FacebookLogin.as_view(), name='fb_login'),
